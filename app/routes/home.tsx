e7008ba69f2f74ab0f4ffc3a3c85ca4c8154d811
nakayamaryo0731 <ryo_nakayama@caddi.jp>
2024-12-02 22:18:39 +0900

    feat: init app

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,7 @@
 export default function IndexPage() {
   return (
     <div style={{ backgroundColor: 'white', color: 'black', display: 'flex', alignItems: 'center' }}>
-      <h1>Hello world</h1>
+      <h1>Hello Home!!</h1>
     </div>
   );
 }
